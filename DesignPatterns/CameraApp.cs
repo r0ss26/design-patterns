7b0a2e046435e0a8c99681af7a5767d2d73e4e8e
Ross Baker <Ross.Baker@hallandwilcox.com.au>
2021-02-28 19:16:38 +1100

    strategy pattern

---
+++ DesignPatterns/CameraApp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+// Strategy pattern
+namespace DesignPatterns
+{
+    public class CameraAppSimulation
+    {
+
+        public interface ShareBehaviour
+        {
+            void Share();
+        }
+
+        public class SocialMediaShare : ShareBehaviour
+        {
+            public void Share()
+            {
+                Console.WriteLine("You just shared a photo on social media");
+            }
+        }
+
+        public class CameraApp
+        {
+            public void Take() { Console.WriteLine("photo taken"); }
+            public void SavePhoto() { Console.WriteLine("photo saved"); }
+
+            public ShareBehaviour ShareBehaviour { get; set; }
+
+        }
+
+        public class BasicCameraApp : CameraApp
+        {
+            public void Edit() { Console.WriteLine("Basic Edit"); }
+        }
+
+        public class CameraPlusapp : CameraApp
+        {
+            public void Edit() { Console.WriteLine("Plus Edit"); }
+        }
+        static void Main(string[] args)
+        {
+            var cameraApp = new BasicCameraApp
+            {
+                ShareBehaviour = new SocialMediaShare()
+            };
+
+            cameraApp.Take();
+            cameraApp.SavePhoto();
+            cameraApp.ShareBehaviour.Share();
+            Console.ReadLine();
+        }
+    }
+}

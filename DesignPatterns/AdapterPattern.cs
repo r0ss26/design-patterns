47720e28a3e79ce0ad769925d1d27b56a84ce4d5
Ross Baker <Ross.Baker@hallandwilcox.com.au>
2021-03-04 16:55:25 +1100

    Adapter pattern challenge solution

---
+++ DesignPatterns/AdapterPattern.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns
+{
+    class AdapterPattern
+    {
+        public interface IDrone
+        {
+            void Beep();
+            void SpinRotors();
+            void TakeOff();
+        }
+
+        public interface IDuck
+        {
+            void Quack();
+            void Fly();
+        }
+
+        public class SuperDrone : IDrone
+        {
+            public void Beep()
+            {
+                Console.WriteLine("Beep beep beep");
+            }
+
+            public void SpinRotors()
+            {
+                Console.WriteLine("Rotors are spinning");
+            }
+
+            public void TakeOff()
+            {
+                Console.WriteLine("Taking Off");
+            }
+        }
+
+        public class DroneAdapter : IDuck
+        {
+            private readonly IDrone drone;
+            public DroneAdapter(IDrone drone)
+            {
+                this.drone = drone;
+            }
+
+            public void Quack()
+            {
+                drone.Beep();
+            }
+
+            public void Fly()
+            {
+                drone.TakeOff();
+            }
+        }
+
+        public class DuckSimulator
+        {
+            private readonly IDuck duck;
+            public DuckSimulator(IDuck duck)
+            {
+                this.duck = duck;
+            }
+
+            public void Run()
+            {
+                duck.Quack();
+                duck.Fly();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            IDrone drone = new SuperDrone();
+            IDuck droneAdapter = new DroneAdapter(drone);
+            DuckSimulator simulation = new DuckSimulator(droneAdapter);
+            simulation.Run();
+        }
+
+    }
+}
---
+++ DesignPatterns/StrategyPattern.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+// Strategy pattern
+namespace DesignPatterns
+{
+    public class CameraAppSimulation
+    {
+
+        public interface IShareBehaviour
+        {
+            void Share();
+        }
+
+        public class SocialMediaShare : IShareBehaviour
+        {
+            public void Share()
+            {
+                Console.WriteLine("You just shared a photo on social media");
+            }
+        }
+
+        public class StrategyPattern
+        {
+            public void Take() { Console.WriteLine("photo taken"); }
+            public void SavePhoto() { Console.WriteLine("photo saved"); }
+
+            public IShareBehaviour ShareBehaviour { get; set; }
+
+        }
+
+        public class BasicCameraApp : StrategyPattern
+        {
+            public void Edit() { Console.WriteLine("Basic Edit"); }
+        }
+
+        public class CameraPlusapp : StrategyPattern
+        {
+            public void Edit() { Console.WriteLine("Plus Edit"); }
+        }
+        //static void Main(string[] args)
+        //{
+        //    var cameraApp = new BasicCameraApp
+        //    {
+        //        ShareBehaviour = new SocialMediaShare()
+        //    };
+
+        //    cameraApp.Take();
+        //    cameraApp.SavePhoto();
+        //    cameraApp.Edit();
+        //    cameraApp.ShareBehaviour.Share();
+        //    Console.ReadLine();
+        //}
+    }
+}
